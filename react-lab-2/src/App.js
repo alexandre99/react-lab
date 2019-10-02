92b30c165e3c62ea56dcafd3577d816fb9147fa3
Alexandre Teixeira <alexanddy99@gmail.com>
2019-10-01 22:47:12 -0300

    iniciado implementação de redux

--- react-lab-2/src/App.js
+++ react-lab-2/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import Header from './componentes/Header';
 import Timeline from './componentes/Timeline';
-import TimelineStore from './stores/TimelineStore';
+import thunkMiddleware from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import { timeline } from './reducers/timeline';
 
-const  timelineStore = new TimelineStore([]);
+const store = createStore(timeline, applyMiddleware(thunkMiddleware));
 
 class App extends Component {
 
@@ -16,7 +18,7 @@ class App extends Component {
       <div id="root">
         <div className="main">
           <Header />
-          <Timeline login={this.props.arg.match.params.login} store={timelineStore}/>
+          <Timeline login={this.props.arg.match.params.login} store={store} />
         </div>
       </div>
     );
